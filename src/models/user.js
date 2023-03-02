193955832eed9a7dd972ed977539221dd202c265
Ignacio casado <nacho.casado01@gmail.com>
2023-03-02 20:27:46 -0300

    keep working on ver 1.0.0 api-ignacio-casado

--- src/models/user.js
+++ src/models/user.js
@@ -10,5 +10,8 @@ const userSchema = new Schema({
 userSchema.methods.encryptPassword = (password) =>{
     return bc.bcrypt.hashSync(password, bcrypt.genSaltSync(10))
 }
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password)
+}
 
 module.exports = mongoose.model('user', userSchema)
